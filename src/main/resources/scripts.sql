000b865532ea520dc0bc79ec954328a0a4a33ec2
Caddarik <cchaveriat@hotmail.fr>
2018-01-25 15:15:12 +0100

    change of script.sql version++

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -3,6 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+ATTACH "[SQLITEDB-HOME]\blog.db" AS blog;
+
 /**
  * Author:  cedric
  * Created: 20 janv. 2018
@@ -23,3 +26,11 @@ create table blog.post (
 	date TEXT NOT NULL,
 	FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE RESTRICT
 ) ;
+
+/*Needed for the tests*/
+insert into user (id, name, email, password) values
+    (1, 'u1', 'u1@domain.se', '123'),
+    (2, 'test', 'test@domain.io', '123');
+
+insert into post (user_id, title, body, date) values
+    (1, 'test', 'Hello world', DATETIME('now'));
